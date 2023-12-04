b8d8e8c207b2b8edabffa24d14a0d070e59a8bda
Can Ivit <canivit@users.noreply.github.com>
2023-12-04 13:01:41 -0500

    implement edit profile

--- src/users/model.ts
+++ src/users/model.ts
@@ -1,59 +1,60 @@
 import mongoose from "mongoose";
 
 export interface User {
+  _id: string;
   username: string;
   password: string;
   firstName: string;
   lastName: string;
   dob: Date;
   role: "Trainer" | "Athlete";
-}
 
-export interface Trainer extends User {
-  background: string;
-  speciality: string;
-}
+  // Trainer properties
+  background?: string;
+  speciality?:
+    | "Bodybuilding"
+    | "Powerlifting"
+    | "Crossfit"
+    | "Endurance"
+    | "Calisthenics";
 
-export interface Athlete extends User {
-  height: number;
-  weight: number;
-  level: "Beginner" | "Intermediate" | "Advanced";
+  // Athlete properties
+  height?: number;
+  weight?: number;
+  level?: "Beginner" | "Intermediate" | "Advanced";
 }
 
 const userSchema = new mongoose.Schema<User>(
   {
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    firstName: String,
-    lastName: String,
-    dob: Date,
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    dob: { type: Date, required: true },
     role: {
       type: String,
+      required: true,
       enum: ["Trainer", "Athlete"],
+    },
+    background: { type: String },
+    speciality: {
+      type: String,
+      enum: [
+        "Bodybuilding",
+        "Powerlifting",
+        "Crossfit",
+        "Endurance",
+        "Calisthenics",
+      ],
+    },
+    height: { type: Number },
+    weight: { type: Number },
+    level: {
+      type: String,
+      enum: ["Beginner", "Intermediate", "Advanced"],
     },
   },
   { collection: "user", discriminatorKey: "role" }
 );
 
 export const userModel = mongoose.model<User>("user", userSchema);
-
-export const trainerModel = userModel.discriminator<Trainer>(
-  "Trainer",
-  new mongoose.Schema({
-    background: String,
-    speciality: String,
-  })
-);
-
-export const athleteModel = userModel.discriminator<Athlete>(
-  "Athlete",
-  new mongoose.Schema({
-    height: Number,
-    weight: Number,
-    level: {
-      type: String,
-      enum: ["Beginner", "Intermediate", "Advanced"],
-    },
-  })
-);
-
--- src/users/routes.ts
+++ src/users/routes.ts
@@ -13,6 +13,7 @@ export function userRoutes(app: Express) {
   app.get("/api/users/signout", signout);
   app.post("/api/users/signin", signin);
   app.post("/api/users/signup", signup);
+  app.post("/api/users/:userId", updateUser);
 }
 
 async function signin(req: Request<{}, {}, Credentials>, res: Response) {
@@ -51,6 +52,30 @@ async function getSignedInUser(req: Request, res: Response) {
 async function signout(req: Request, res: Response) {
   req.session.destroy(() => {});
   res.sendStatus(200);
+}
+
+async function updateUser(
+  req: Request<{ userId: string }, {}, User>,
+  res: Response
+) {
+  const { userId } = req.params;
+  if (
+    req.session.currentUser === undefined ||
+    req.session.currentUser._id !== userId
+  ) {
+    res.status(401).send("Not signed in");
+    return;
+  }
+
+  const user = await dao.findUserById(userId);
+  if (!user) {
+    res.status(404).send("User not found");
+    return;
+  }
+
+  await dao.updateUser(userId, req.body);
+  req.session.currentUser = (await dao.findUserById(userId))!;
+  res.json(req.session.currentUser);
 }
 
 type Credentials = {
